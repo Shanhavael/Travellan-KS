aa33374759922715d2ad31410918eeb626fabbcc
Shanhavael <konrad.skrzypczak1@op.pl>
2021-02-08 19:48:35 +0100

    TRIPP-255 connect with API

---
+++ src/services/fetchMapRoute.js
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import { MAPBOX_API_KEY } from 'react-native-dotenv';
+
+const API_KEY = MAPBOX_API_KEY;
+
+async function fetchMapSearch(points) {
+  return await axios
+    .get(
+      `https://api.mapbox.com/directions/v5/mapbox/driving/${points}?access_token=${API_KEY}`,
+    )
+    .then((json) => json.data);
+}
+
+export default fetchMapSearch;
